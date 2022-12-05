c1e427bec5ad781cd55574faafaa4a0a0432e3f0
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2022-12-05 10:13:42 +0200

    Added javascript code on app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+const noteListDiv = document.querySelector('note-list');
+let noteID = 1;
+function Note(id, title, content){
+    this.id = id;
+    this.title = title;
+    this.content = content;
+}
+
+//all eentListeners
+function eventListeners() {
+    document.getElementById('add-note-btn').addEventListener('click', addNewNote);
+}
+
+eventListeners();
+
+// add a new note in the list
+function addNewNote(){
+    const noteTitle = document.getElementById('note-title'),
+    noteContent = documents.getElementById('note-content');
+    
+    if(validateInput(noteTitle, noteContent)) {
+        let notes = [];
+        let noteItem = new Note(noteID, noteTitle.value, noteContent.value);
+        noteID++;
+        notes.push(noteItem);
+    }
+}
+
+// input validation
+function validateInput(title, content) {
+    if(title.value !== "" && content.value !== ""){
+        return true;
+    } else {
+        if(title.value === "") title.classList.add('warning');
+        if(content.value === "") content.classList.add('warning');
+    }
+    setTimeout(() => {
+        title.classList.remove('warning');
+        content.classList.remove('warning');
+    }, 1500);
+}
